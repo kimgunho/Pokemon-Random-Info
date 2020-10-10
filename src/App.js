4873305053b86553e047470d1fccbc5b644bb484
kimgunho <kimgunho@kimgunhoui-MacBook-Pro.local>
2020-10-10 18:50:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="pokemon">
+      포켓몬 api
+    </div>
+  );
+}
+
+export default App;
